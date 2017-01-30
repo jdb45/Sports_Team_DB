62f7ce3e35d6a2ef7e93449422f8e557f191e82a
Jeremy <acnajcadcac.yahoo.com>
2017-01-29 23:35:40 -0600

    Created a file to create the tables and columns for the DB

---
+++ sports_team_tables.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Column, Integer, String, REAL
+
+from base import Base
+
+
+class Games(Base):
+
+
+   # creating a table
+    __tablename__ = 'games'
+
+    # creating the columns
+    date = Column(String, primary_key=True)
+    opponent_team = Column(String)
+    location = Column(String)
+
+    def __repr__(self):
+        return 'games: date = {} opponent_team = {} location = {}'.format(self.date, self.opponent_team, self.location)
+
+
+class Sales(Base):
+
+
+   # creating a table
+    __tablename__ = 'sales'
+
+    # creating the columns
+    id = Column(Integer, primary_key=True)
+    total_sales = Column(REAL)
+    jersey_sales = Column(REAL)
+    hat_sales = Column(REAL)
+    poster_sales = Column(REAL)
+    date_sales = Games.date
+
+
+    def __repr__(self):
+        return 'sales: id = {} total_sales = {} jersey_sales = {} hat_sales = {} poster_sales = {} date = {}'\
+            .format(self.id, self.total_sales, self.jersey_sales, self.hat_sales, self.poster_sales, self.date_sales)
+
+
+class Merchandise(Base):
+
+
+   # creating a table
+    __tablename__ = 'merchandise'
+
+    # creating the columns
+    id = Column(Integer, primary_key=True)
+    jerseys = Column(Integer)
+    hats = Column(Integer)
+    posters = Column(Integer)
+    date_merchandise = Games.date
+
+
+    def __repr__(self):
+        return 'merchandise: id = {} jerseys = {} hats = {} posters = {} date = {}'\
+            .format(self.id, self.jerseys, self.hats, self.posters, self.date_merchandise)
