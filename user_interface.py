79cea2222e58e224b8d156b2babf943b5ddb449d
Jeremy <acnajcadcac.yahoo.com>
2017-01-29 21:17:29 -0600

    First commit! added the beginning of the UI and started figuring out class files

---
+++ user_interface.py
@@ -0,0 +1,38 @@
+
+
+def open_menu():
+
+    print('''
+        1. Star Selling!
+        2. View all sales
+        3. Delete ALL data
+        4. View top selling item
+        5. View worst selling items
+        6. Top grossing game
+        q. Quit
+    ''')
+
+    choice = input('Enter your selection: ')
+
+    return choice
+
+
+def get_date():
+
+    user_date = input("Please enter the date of the game: ")
+
+    return user_date
+
+
+def selling_menu():
+
+    print('''
+        1. Sell shirt
+        2. Sell poster
+        3. Sell hat
+        q. Quit
+    ''')
+
+    choice = input('Enter your selection: ')
+
+    return choice
