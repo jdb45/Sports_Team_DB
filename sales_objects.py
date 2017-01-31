e91d88a5e28766d29851dbb7e3b130ce056081f9
Jeremy <acnajcadcac.yahoo.com>
2017-01-31 15:40:36 -0600

    Created a file to use for objects for sale items. Created a global list to store the prices and counts for the items sold. Created functions for each item for sale.

---
+++ sales_objects.py
@@ -0,0 +1,6 @@
+
+class Sales_Object:
+
+    def __init__(self, total, count):
+        self.total = total
+        self.count = count
--- sales_orm.py
+++ sales_orm.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from base import Base
 import user_interface
+from sales_objects import Sales_Object
 from sports_team_tables import Games, Sales, Merchandise
 
 
@@ -30,7 +31,10 @@ def handle_choice_selling(choice):
     # menu handler for selling screen
 
     if choice == '1':
-        print()
+        sell_jersey()
+
+    elif choice == '2':
+        sell_poster()
 
     elif choice == 'q':
         main()
@@ -44,12 +48,41 @@ def selling_screen():
     location_game = user_interface.get_location()
     opponent_game = user_interface.get_opponent()
 
+    save_session = Session()
+
     quit = 'q'
     choice = None
+
+    global sales_list
+    sales_list = [0,0,0,0,0,0]
 
     while choice != quit:
         choice = user_interface.selling_menu()
         handle_choice_selling(choice)
+
+
+def sell_jersey():
+    global sales_list
+    Sales_Object.total = 50
+    Sales_Object.count = 1
+    sales_list[0] += Sales_Object.total
+    sales_list[1] += Sales_Object.count
+
+
+def sell_poster():
+    global sales_list
+    Sales_Object.total = 5
+    Sales_Object.count = 1
+    sales_list[2] += Sales_Object.total
+    sales_list[3] += Sales_Object.count
+
+
+def sell_hat():
+    global sales_list
+    Sales_Object.total = 20
+    Sales_Object.count = 1
+    sales_list[4] += Sales_Object.total
+    sales_list[5] += Sales_Object.count
 
 
 def main():
